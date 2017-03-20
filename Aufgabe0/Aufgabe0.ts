cd3073cf5a0e6c466f5eb3f77cadef5f37ac6141
matth <matth@LAPTOP-9O67TI3N>
2017-03-20 14:12:39 +0100

    aufgabe0

---
+++ Aufgabe0/Aufgabe0.ts
@@ -0,0 +1,16 @@
+ 
+//Aufgabe: 0
+//Name: Matthias Fischer
+//Matrikel: 255035
+//Datum: 19.03.17
+    
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var name = prompt("Bitte geben Sie Ihren Namen ein.");
+    var gruss = "Herzlich Willkommen ";
+    document.body.innerHTML = gruss + name;
+});
